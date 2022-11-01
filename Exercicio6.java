a58c05f2ebb4bfdc90947039eece416f4f07499e
Lucas Martins <64702398+APONTES19@users.noreply.github.com>
2022-11-01 11:59:56 -0300

    Add files via upload

---
+++ Exercicio6.java
@@ -0,0 +1,26 @@
+
+public class Exercicio6 {
+
+    public static void main(String args[]) 
+    {
+        int a;
+        int b;
+        
+        a =0;
+        b = 15;
+        while (a <= b)
+        {
+            System.out.println("O Valor de A dentro do while é:" + a);
+            a++;
+        }
+        
+        a =0;
+        b = 15;
+        while (b >= a)
+        {
+            System.out.println("O Valor de B dentro do while é:" + b);
+            b--;
+        }
+        
+    }
+}
