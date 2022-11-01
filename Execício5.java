e8123f7c3154c35f18b0fd7eb2a519315a029b6f
Lucas Martins <64702398+APONTES19@users.noreply.github.com>
2022-11-01 11:49:46 -0300

    Add files via upload

---
+++ Execício5.java
@@ -0,0 +1,23 @@
+
+public class Execício5
+{
+    public static void main(String args[])
+    {
+        int a;
+        int b;
+        
+        a = 0;
+        b = 10;
+        
+        for (a = 0; a<= b; a++)
+        {
+            System.out.println("Incremento de a até b, O valor no FOR é: " + a);
+        }
+        a = 0;        
+        for (b = 10; b >= a; b--)
+        {
+            System.out.println("Decremento de B até a, o valor no FOR é:" + b);
+        }
+    }
+}
+//
