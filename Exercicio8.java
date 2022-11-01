f2ebf7b953ea99f1b475080f93013fe196322d62
Lucas Martins <64702398+APONTES19@users.noreply.github.com>
2022-11-01 14:50:28 -0300

    Add files via upload

---
+++ Exercicio8.java
@@ -0,0 +1,17 @@
+public class Exercicio8
+{
+    public static void main(String args[])
+    {
+        int a;
+        int b;
+        
+        a = 5;
+        b = 2;        
+        System.out.println("\nSe A = " + a + " for maior do que B = " + b + " Imprima A se não imprima B\na>b ? a:b");
+        System.out.println(a>b?a:b);
+        a = 2;
+        b = 8;
+        System.out.println("\nSe A = " + a + " for maior do que B = " + b + " Imprima A se não imprima B\na>b ? a:b");
+        System.out.println(a>b?a:b);
+    }
+}
